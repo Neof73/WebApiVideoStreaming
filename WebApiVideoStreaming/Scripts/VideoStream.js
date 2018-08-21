9b220740c063e802aed540db3906865b620d753f
Mario Di Giorgio <mario.digiorgio@globant.com>
2018-08-21 18:43:32 -0300

    Upload progress...

--- WebApiVideoStreaming/Scripts/VideoStream.js
+++ WebApiVideoStreaming/Scripts/VideoStream.js
@@ -1,6 +1,12 @@
 ﻿$(document).ready(function () {
+
     $(':file').on('change',
         function (event, numFiles, label) {
+            var sizeInKb = parseFloat($(this).prop("files")['0'].size / 1024).toFixed(2);
+            var fileName = $(this).prop("files")['0'].name;
+            uploadProgress = $('#dvProgress');
+            $(uploadProgress).attr("aria-valuemax", sizeInKb);
+            $(uploadProgress).css("width", "0%");
 
             var input = $(this).parents('.input-group').find(':text'),
                 log = event.target.files[0].name;
@@ -13,4 +19,61 @@
             }
 
         });
+
+
+    $('#uploadForm').submit(function (e) {
+
+        var formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/videos/mediaupload',
+            data: formData,
+            xhr: function () {
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) {
+                    myXhr.upload.addEventListener('progress', progress, false);
+                }
+                return myXhr;
+            },
+            cache: false,
+            contentType: false,
+            processData: false,
+
+            success: function (data) {
+                console.log(data);
+                $('#dvProgress').addClass("progress-bar-success");
+                location.reload();
+            },
+
+            error: function (data) {
+                console.log(data);
+            }
+        });
+
+        e.preventDefault();
+
+    });
+
 });
+
+
+function progress(e) {
+
+    if (e.lengthComputable) {
+        var max = e.total;
+        var current = e.loaded;
+
+        var Percentage = (current * 100) / max;
+        console.log(Percentage);
+
+        uploadProgress = $('#dvProgress');
+        $(uploadProgress).attr("aria-valuemax", max);
+        $(uploadProgress).attr("aria-valuenow", current);
+        $(uploadProgress).css("width", Percentage + "%");
+
+        if (Percentage >= 100) {
+            // process completed  
+        }
+    }
+}
