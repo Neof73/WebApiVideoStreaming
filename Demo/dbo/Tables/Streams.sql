34671f9455e2ff25279367c87f2259aef8ef7458
Mario Di Giorgio <mdigiorgio@deloitte.com>
2018-08-21 14:15:19 -0500

    New database proyect

---
+++ Demo/dbo/Tables/Streams.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Streams] (
+    [id]       BIGINT          IDENTITY (1, 1) NOT NULL,
+    [textdata] NVARCHAR (MAX)  NULL,
+    [bindata]  VARBINARY (MAX) NULL,
+    CONSTRAINT [PK_Streams_1] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
