2da03d1bd25995a8cd36d20377b434bcd1ce5f59
Mario Di Giorgio <mario.digiorgio@globant.com>
2018-08-23 17:57:49 -0300

    Nothing

--- WebApiVideoStreaming/Support/FileStreamer.cs
+++ WebApiVideoStreaming/Support/FileStreamer.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace WebApiVideoStreaming.Support
 {
